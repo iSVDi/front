e833ce64b2c443eb39801e7845cba2d686bcf236
Daniil Shestopalov <d.shestopalov@letmecode.org>
2025-03-12 21:00:20 +0300

    added styles

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./css/App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Router, Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/favorites" element={<Favorites />}></Route>
-      </Routes>
-    </main>
+      <Navbar></Navbar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
