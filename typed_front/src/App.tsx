60ba63b3ef0db7eec531d52a91b3d1eb2e339d59
Daniil Shestopalov <d.shestopalov@letmecode.org>
2025-03-22 15:15:19 +0300

    try add routing

--- typed_front/src/App.tsx
+++ typed_front/src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./css/App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { Route, Routes, Router, Link, BrowserRouter } from "react-router-dom";
+import Favorites from "./pages/Favorites";
 
 function App() {
-  return <div>
-    <Navbar></Navbar>
-    <Home />;
-  </div>
-  
+  // return <Home></Home>
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
